75d4f44e2ad400388077d4c49928988b831c3d1c
Tom <q.tom08@gmail.com>
2023-01-05 02:23:32 -0500

    Add Boost as a dependency

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zbqi/Documents/network-monitor
+# Install script for directory: /Users/zbqi/documents/network-monitor
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zbqi/Documents/network-monitor/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zbqi/documents/network-monitor/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
